fc082f2b6fc382019e1a5e562bc93fcc61b151f7
rmhirota <re.hirota@gmail.com>
2020-12-08 19:36:08 -0300

    tidy

--- data-raw/1_tidy.R
+++ data-raw/1_tidy.R
@@ -14,18 +14,27 @@ devtools::load_all()
 
 # Xingamentos -------------------------------------------------------------
 
-a <- monitora::da_tidy %>%
-  dplyr::select(-xingamento) %>%
-  add_xingamentos()
-da_tidy <- a
-
-usethis::use_data(da_tidy, overwrite = TRUE)
-
-
-da_xingamento <- da_tidy %>%
-  dplyr::filter(xingamento == "Sim") %>%
-  add_lista_xingamentos()
-
-usethis::use_data(da_xingamento, overwrite = TRUE)
-
-
+# aa <- dplyr::bind_rows(
+#   readr::read_rds("~/Desktop/data-raw/da_tw_tidy.rds"),
+#   readr::read_rds("~/Desktop/data-raw/da_yt_tidy.rds")
+# )
+#
+# aa <- monitora::da_tidy %>%
+#   dplyr::filter(!rede_social %in% c("twitter", "youtube")) %>%
+#   dplyr::bind_rows(aa)
+#
+# da_tidy <- aa
+#
+# usethis::use_data(da_tidy, overwrite = TRUE, compress = "xz")
+#
+#
+# da_xingamento <- da_tidy %>%
+#   dplyr::filter(xingamento == "Sim") %>%
+#   add_lista_xingamentos()
+#
+# usethis::use_data(da_xingamento, overwrite = TRUE)
+#
+#
+# da_xingamento <- readr::read_rds("~/Desktop/data-raw/da_xingamento.rds")
+# usethis::use_data(da_xingamento, overwrite = T)
+# devtools::document()
